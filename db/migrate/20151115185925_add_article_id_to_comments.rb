e32ab437c85e01b724c8dc545179209b941433a6
Dorian Lupu <dorian.lupu@gmail.com>
2015-11-15 20:03:58 +0100

    create association between comments and models
    rails generate migration AddArticleIdToComments article:references

---
+++ db/migrate/20151115185925_add_article_id_to_comments.rb
@@ -0,0 +1,5 @@
+class AddArticleIdToComments < ActiveRecord::Migration
+  def change
+    add_reference :comments, :article, index: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151115183117) do
+ActiveRecord::Schema.define(version: 20151115185925) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 20151115183117) do
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "article_id"
   end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
