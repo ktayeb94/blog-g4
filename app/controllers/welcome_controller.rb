904ffa13b122d808f308908879f5484f96fce140
Dorian Lupu <dorian.lupu@gmail.com>
2015-11-17 10:35:50 +0100

    be able to switch between locales

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,14 @@ class WelcomeController < ApplicationController
   def index
     @articles = Article.order(created_at: :desc).limit(10)
   end
+
+  def change_locale
+    session[:locale] = if params[:locale].in? ['en', 'fr']
+                         params[:locale].to_sym
+                       else
+                         nil
+                       end
+    # not suitable for production but useful for this course
+    redirect_to :back
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
   end
 
   resource :user, :only => [:edit, :update]
+
+  get 'change_locale/:locale' => "welcome#change_locale", :as => "change_locale"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
