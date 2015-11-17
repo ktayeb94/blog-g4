65555cebcd49e453c89483b7a387ef2446c53fde
Dorian Lupu <dorian.lupu@gmail.com>
2015-11-17 08:44:30 +0100

    display a list of featured blogs under the navigation menu (featured blog = blog that has at least one article)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_featured_blogs
+
+  protected
+
+  #returns blogs that have at least one articles
+  def set_featured_blogs
+    # joins uses an inner join by default so using User.joins(:articles)
+    # will in effect only return users that have an associated articls.
+    @featured_users = User.joins(:articles).uniq
+  end
 end
